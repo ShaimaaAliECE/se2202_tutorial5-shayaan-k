e4ea2607cdfd26a3f9be47b15cc82de5fcc608c9
Shayaan <skhatri7@uwo.ca>
2021-10-18 21:54:08 -0400

    first commit

--- scripts.js
+++ scripts.js
@@ -3,13 +3,22 @@ let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the gam
 //initialize the game
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
-
+let nextLabel = document.getElementById("next-lbl");
+nextLabel.innerText = nextPlayer;
 
 //This call will create the buttons needed for the gameboard.
 createGameBoard()
 
 function createGameBoard()
 {
+    for(let i=0;i<9;i++){
+        let button = document.createElement('button');
+        button.innerText="[ ]";
+
+        let buttonElement = document.getElementById("c" + (i+1));
+
+        buttonElement.appendChild(button);
+    }
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
    
 }
@@ -27,13 +36,29 @@ function takeCell(event)
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
-
+   let btn = event.target;
+   btn.innerText = '[' + nextPlayer + ']';
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
+
+    btn.disabled = true;
+
+    if(nextPlayer ==='X'){
+        nextPlayer = 'O';
+    }
+    else if (nextPlayer === 'O'){
+        nextPlayer = 'X';
+    }
+
+    playerTitle.innerText = nextPlayer;
 
     // Check if the game is over
     if (isGameOver())
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        var gameOverLbl = document.getElementById('game-ever-lbl');
+        var gameOverText = document.createElement('h1');
+        gameOverText.innerText = 'Game Over';
+        gameOverLbl.appendChild(gameOverText);
     }
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
@@ -42,5 +67,12 @@ function takeCell(event)
 function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
-   
+   let allBtnUsed = true;
+
+   for (let i=0;i<btns.length;i++){
+       if (!btns[i].disabled){
+           allBtnUsed = false;
+       }
+   }
+   return allBtnUsed;
 }
